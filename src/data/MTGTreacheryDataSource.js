716fbd51b34f8d4b6ff8d6feb5c987a3845b6684
Hunter Larco <hunter@larcolabs.com>
2022-05-19 20:17:39 -0700

    fetch data from mtgtreachery.net

--- src/data/MTGTreacheryDataSource.js
+++ src/data/MTGTreacheryDataSource.js
@@ -1,5 +1,29 @@
+const fetch = require('node-fetch');
+
+async function fetchIdentities() {
+  const { cards } = await fetch(
+    'https://mtgtreachery.net/rules/oracle/treachery-cards.json'
+  ).then((response) => response.json());
+
+  return {
+    assassins: cards.filter((card) => card.types.subtype == 'Assassin'),
+    guardians: cards.filter((card) => card.types.subtype == 'Guardian'),
+    leaders: cards.filter((card) => card.types.subtype == 'Leader'),
+    traitors: cards.filter((card) => card.types.subtype == 'Traitor'),
+  };
+}
+
 class IdentityDataSource {
-  async getIdentities() {}
+  constructor() {
+    this._identities = null;
+  }
+
+  async getIdentities() {
+    if (!this._identities) {
+      this._identities = await this._fetchIdentities();
+    }
+    return this._identities;
+  }
 }
 
 module.exports = {
